fc55c82f5dfce0c8407caeaa4a26c01b01741ca0
Sitian Chen <sitianchen@Sitians-MacBook-Pro-2.local>
2023-03-05 23:32:55 -0800

    a2-consumer1

--- Assignment2/assignment2_consumer1/src/main/java/Consumer1.java
+++ Assignment2/assignment2_consumer1/src/main/java/Consumer1.java
@@ -13,13 +13,12 @@ public class Consumer1 implements Runnable{
   private static final String QUEUE = "swipe_queue";
   private static final int LIKES_INDEX = 0;
   private static final int DISLIKES_INDEX = 1;
-  private int number_req_each_thread;
   private Connection connection;
   private ConcurrentHashMap<String, int[]> swipeMap;
 
-  public Consumer1(int number_req_each_thread, Connection connection,
+  public Consumer1(Connection connection,
       ConcurrentHashMap<String, int[]> swipeMap) {
-    this.number_req_each_thread = number_req_each_thread;
+
     this.connection = connection;
     this.swipeMap = swipeMap;
   }
@@ -27,44 +26,38 @@ public class Consumer1 implements Runnable{
 
   @Override
   public void run() {
-    for (int i = 0; i < number_req_each_thread; i++) {
-      try {
-        final Channel channel = connection.createChannel();
-        channel.queueDeclare(QUEUE, true, false, false, null);//second true or false??
-        channel.queueBind(QUEUE, "exchange", "");
-        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    try {
+      final Channel channel = connection.createChannel();
+      channel.queueDeclare(QUEUE, true, false, false, null);//second true or false??
+      channel.queueBind(QUEUE, "exchange", "");
+      System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
 
-        // accept only 1 unacknowledged message
-        channel.basicQos(1);
+      // accept only 1 unacknowledged message
+      channel.basicQos(1);
 
-        Gson gson = new Gson();//pass gson from main?
-        DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-          String message = new String(delivery.getBody(), "UTF-8");
+      Gson gson = new Gson();//pass gson from main?
+      DeliverCallback deliverCallback = (consumerTag, delivery) -> {
+        String message = new String(delivery.getBody(), "UTF-8");
 
-          SwipeData swipeData = (SwipeData) gson.fromJson(message, SwipeData.class);
-          String swiper = swipeData.getSwipeDetails().getSwiper();
+        SwipeData swipeData = (SwipeData) gson.fromJson(message, SwipeData.class);
+        String swiper = swipeData.getSwipeDetails().getSwiper();
 
-          swipeMap.putIfAbsent(swiper, new int[] {0,0});
-          if (swipeData.getLeftOrRight().equals("right")) {
-//            swipeMap.getOrDefault(swiper, new int[]{0,0})[LIKES_INDEX]++;
-            swipeMap.get(swiper)[LIKES_INDEX]++;
-          } else if (swipeData.getLeftOrRight().equals("left")) {
-//            swipeMap.getOrDefault(swiper, new int[]{0,0})[DISLIKES_INDEX]++;
-            swipeMap.get(swiper)[DISLIKES_INDEX]++;
-          }
+        swipeMap.putIfAbsent(swiper, new int[] {0,0});
+        if (swipeData.getLeftOrRight().equals("right")) {
+          swipeMap.get(swiper)[LIKES_INDEX]++;
+        } else if (swipeData.getLeftOrRight().equals("left")) {
+          swipeMap.get(swiper)[DISLIKES_INDEX]++;
+        }
 
-          channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
+        channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
 //          System.out.println(" [x] Received '" + message + "'");
-          System.out.println(swiper + " : " + swipeMap.get(swiper)[0] + " " + swipeMap.get(swiper)[1]);
+        System.out.println(swiper + " : " + swipeMap.get(swiper)[0] + " " + swipeMap.get(swiper)[1]);
+      };
 
-        };
+      channel.basicConsume(QUEUE, false, deliverCallback, consumerTag -> { });
 
-        channel.basicConsume(QUEUE, false, deliverCallback, consumerTag -> { });
-
-      } catch (IOException e) {
-        e.printStackTrace();
-      }
-
+    } catch (IOException e) {
+      e.printStackTrace();
     }
 
   }
--- Assignment2/assignment2_consumer1/src/main/java/Main1.java
+++ Assignment2/assignment2_consumer1/src/main/java/Main1.java
@@ -10,8 +10,8 @@ import java.util.concurrent.CountDownLatch;
 
 
 public class Main1 {
-  private static final Integer NUM_TOTAL_CONSUMER_THREADS = 50;
-  private static final Integer NUM_CONSUMERS_EACH_THREAD = 1;
+  private static final Integer NUM_TOTAL_CONSUMER_THREADS = 100;
+//  private static final Integer NUM_CONSUMERS_EACH_THREAD = 1;
   private static ConcurrentHashMap<String, int[]> swipeMap;
   private static final String RMQ_LOCAL_HOST = "localhost";
   private static final String RMQ_REMOTE_HOST = "44.242.45.40";
@@ -31,32 +31,8 @@ public class Main1 {
     Connection connection = factory.newConnection();
 
     for (int i = 0; i<NUM_TOTAL_CONSUMER_THREADS; i++) {
-      new Thread(new Consumer1(NUM_CONSUMERS_EACH_THREAD, connection, swipeMap)).start();
+      new Thread(new Consumer1(connection, swipeMap)).start();
     }
-
-    System.out.println("---------------------------------");
-//    for (Map.Entry<String, int[]> entry : swipeMap.entrySet()) {
-//      System.out.println(entry.getKey() + ":" + entry.getValue()[0] + " " + entry.getValue()[1]);
-//    }
-
-
-//    Channel channel = connection.createChannel();
-//    channel.queueDeclare("c1", false, false, false, null);
-//    channel.queueBind("c1", "exchange", "");
-//
-//
-//
-//    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-//
-//    // accept only 1 unacknowledged message
-//    channel.basicQos(1);
-//
-//    DeliverCallback deliverCallback = (consumerTag, delivery) -> {
-//      String message = new String(delivery.getBody(), "UTF-8");
-//      channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
-//      System.out.println(" [x] Received '" + message + "'");
-//    };
-//    channel.basicConsume("c1", false, deliverCallback, consumerTag -> { });
   }
 
 }
